85d50705b1467bf187df6caf83593f8ab4d77038
diego41071 <alexander.diego.2007@hotmail.com>
2021-04-15 10:31:46 -0500

    cambios para aplicar testing con jest

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 import { Button, Card, Grid } from "semantic-ui-react";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
-export const App = () => {
+export function App() {
   const [films, setFilms] = useState({});
   const [people, setPeople] = useState([]);
   const [vehicles, setVehicles] = useState([]);
@@ -90,7 +90,15 @@ export const App = () => {
         {visiblefilm && (
           <Button onClick={() => btnFilms()}>Obtener Películas</Button>
         )}
-        <Grid columns={3}>
+        <Grid columns={3} stackable>
+          {visiblefilm &&
+            [1, 2, 3, 4, 5, 6].map((item, index) => {
+              return (
+                <Grid.Column key={index}>
+                  <Card>{item}</Card>
+                </Grid.Column>
+              );
+            })}
           {films?.results?.map((item, index) => {
             return (
               <Grid.Column>
@@ -104,6 +112,97 @@ export const App = () => {
           <Button onClick={() => btnPeople()}>Obtener Personajes</Button>
         )}
         <Grid columns={3} stackable>
+          {visiblepeople &&
+            [
+              1,
+              2,
+              3,
+              4,
+              5,
+              6,
+              7,
+              8,
+              9,
+              10,
+              11,
+              12,
+              13,
+              14,
+              15,
+              16,
+              17,
+              18,
+              19,
+              20,
+              21,
+              22,
+              23,
+              24,
+              25,
+              26,
+              27,
+              28,
+              29,
+              30,
+              31,
+              32,
+              33,
+              34,
+              35,
+              36,
+              37,
+              38,
+              39,
+              40,
+              41,
+              42,
+              43,
+              44,
+              45,
+              46,
+              47,
+              48,
+              49,
+              50,
+              51,
+              52,
+              53,
+              54,
+              55,
+              56,
+              57,
+              58,
+              59,
+              60,
+              61,
+              62,
+              63,
+              64,
+              65,
+              66,
+              67,
+              68,
+              69,
+              70,
+              71,
+              72,
+              73,
+              74,
+              75,
+              76,
+              77,
+              78,
+              79,
+              80,
+              81,
+              82,
+            ].map((item, index) => {
+              return (
+                <Grid.Column key={index}>
+                  <Card>{item}</Card>
+                </Grid.Column>
+              );
+            })}
           {people?.map((item, index) => {
             return (
               <>
@@ -122,7 +221,52 @@ export const App = () => {
         {visiblevehicle && (
           <Button onClick={() => btnVehicles()}>Obtener Naves</Button>
         )}
-        <Grid columns={3}>
+        <Grid columns={3} stackable>
+          {visiblevehicle &&
+            [
+              1,
+              2,
+              3,
+              4,
+              5,
+              6,
+              7,
+              8,
+              9,
+              10,
+              11,
+              12,
+              13,
+              14,
+              15,
+              16,
+              17,
+              18,
+              19,
+              20,
+              21,
+              22,
+              23,
+              24,
+              25,
+              26,
+              27,
+              28,
+              29,
+              30,
+              31,
+              32,
+              33,
+              34,
+              35,
+              36,
+            ].map((item, index) => {
+              return (
+                <Grid.Column key={index}>
+                  <Card>{item}</Card>
+                </Grid.Column>
+              );
+            })}
           {vehicles?.map((item, index) => {
             return (
               <>
@@ -140,4 +284,4 @@ export const App = () => {
       </div>
     </div>
   );
-};
+}
